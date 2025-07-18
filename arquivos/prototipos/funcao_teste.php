7215a27c95279fc31e3ca75414413af21fa9a2a1
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-07-18 20:12:46 -0300

    Add files via upload

---
+++ arquivos/prototipos/funcao_teste.php
@@ -0,0 +1,23 @@
+<?php
+    include 'conexao.php';
+    include 'listar.produtos.php';
+    //todos os produtos
+    listar_produto();
+
+    //produtos masculinos
+    listar_produto_masculino();
+
+    //produtos femininos
+    listar_produto_feminino();
+
+
+
+
+
+
+
+
+
+
+
+?>
